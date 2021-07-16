5e56f934015afacac145c326d379a11ed4a3ed02
Natalia Romashko <sharkellgm@gmail>
2021-07-16 12:28:32 +0300

    refactor: restyle statistics page

--- client/src/components/recordPage/recordPage.ts
+++ client/src/components/recordPage/recordPage.ts
@@ -4,75 +4,65 @@ import ButtonDefault from '../buttonDefault/buttonDefault';
 import Control from '../utilities/control';
 import Record from './record';
 import recordStyles from './recordPage.module.css';
+import RecordHeader from './recordHeader';
+import recordBg from '../../assets/records_bg.png';
 
 export interface IGameRecord {
-  gameType:string;
+  gameType: string;
   date: string;
   player1: IPublicUserInfo;
   player2: IPublicUserInfo;
   winner: string;
   time: string;
-  history:Array<IChessHistory>|Array<ICrossHistory>;
-  gameMode:string;
-  moves:Array<{ field: string; player: string; history: IChessHistory}>
+  history: Array<IChessHistory> | Array<ICrossHistory>;
+  gameMode: string;
+  moves: Array<{ field: string; player: string; history: IChessHistory }>;
 }
 
-
-const tableHeader = ['Game','Date', 'Players', 'Winner', 'Game mode', 'Time/Watch Replay'];
+const tableHeader = [ 'Game', 'Date', 'Players', 'Winner', 'Game mode', 'Time/Watch Replay' ];
 
 class RecordPage extends Control {
   private chessRecordBlock: Control;
   private recordChess: Array<Record>;
-  model : RecordModel = new RecordModel();
+  model: RecordModel = new RecordModel();
+  private tableBody: Control;
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', recordStyles.record_wrapper);
+    this.node.style.backgroundImage = `url(${recordBg})`;
     const title = new Control(this.node, 'div', recordStyles.record_title);
     title.node.textContent = 'Game Statistics';
-    
+
     this.chessRecordBlock = new Control(this.node, 'div', recordStyles.record_table_wrapper);
-    const chessTitle = new Control(this.chessRecordBlock.node, 'div', );
+    const tableHeader = new RecordHeader(this.chessRecordBlock.node);
+    
     const refreshBtn = new ButtonDefault(this.node, recordStyles.record_button, 'Refresh');
     refreshBtn.onClick = () => {
       this.model.getStatistic().then(async (res: Array<IGameRecord>) => {
+        this.tableBody.destroy();
         this.addRecordToChess(res);
-      })
-    }
-    chessTitle.node.textContent = 'Chess';
-    const chessTableTitle = new Control(
-      this.chessRecordBlock.node,
-      'div',
-      recordStyles.record_table_header
-    );
-    tableHeader.forEach((item) => {
-      const headerItem = new Control(chessTableTitle.node, 'div', recordStyles.table_item_header);
-      headerItem.node.textContent = item;
-    })
+      });
+    };
+
+    
 
     this.model.getStatistic().then(async (res: Array<IGameRecord>) => {
       this.addRecordToChess(res);
-    })
+    });
   }
 
   async addRecordToChess(records: Array<IGameRecord>): Promise<void> {
-    this.chessRecordBlock.node.innerHTML='';
-        const chessTableTitle = new Control(
-          this.chessRecordBlock.node,
-          'div',
-          recordStyles.record_table_header
-        );
-        tableHeader.forEach((item) => {
-          const headerItem = new Control(chessTableTitle.node, 'div', recordStyles.table_item_header);
-          headerItem.node.textContent = item;
-        })
-        const stats = await records;
-        stats.forEach((game, index) => {
-          if (index % 2 !== 0) {
-            const recordOdd = new Record(this.chessRecordBlock.node,game);
-            recordOdd.node.classList.add(recordStyles.odd_record);
-          } else {
-            new Record(this.chessRecordBlock.node,game);
-          }
-        })
+    const stats = await records;
+    this.tableBody = new Control(this.chessRecordBlock.node);
+    stats.forEach((game, index) => {
+      if (index % 2 !== 0) {
+        const recordOdd = new Record(this.tableBody.node, game);
+        recordOdd.node.classList.add(recordStyles.odd_record);
+      } else {
+        const recordEven = new Record(this.tableBody.node, game);
+        recordEven.node.classList.add(recordStyles.even_record);
+        
+      }
+    });
   }
 
   hide(): void {
